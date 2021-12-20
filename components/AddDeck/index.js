4551f4ce65417080f5f9ba0d04936ce5e91370a5
dacitto <dacidigital@gmail.com>
2021-12-20 23:13:21 +0100

    feat: add TouchableWithoutFeedback to dismiss keyboard on blur

--- components/AddDeck/index.js
+++ components/AddDeck/index.js
@@ -1,37 +1,49 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, TextInput, Button, Alert } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Touchable,
+} from "react-native";
 import { addDeck } from "../../redux/decksSlice";
 import { useDispatch } from "react-redux";
 const AddDeck = () => {
   const [title, setTitle] = useState("");
   const dispatch = useDispatch();
   return (
-    <View style={styles.container}>
-      <Text>What's the Title of your Deck?</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={(title) => setTitle(title)}
-        placeholder="New deck Title"
-      />
-      <Button
-        onPress={() => {
-          if (title.trim() !== "") {
-            dispatch(
-              addDeck({ id: Date.now(), title: title.trim(), questions: [] })
-            );
-          } else {
-            Alert.alert(
-              "Deck Title is empty!",
-              "Deck Title must contain at least 1 character",
-              [{ text: "ok" }]
-            );
-          }
-        }}
-        title="Create a deck"
-        style={styles.button}
-        accessibilityLabel="Add a new deck"
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text>What's the Title of your Deck?</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(title) => setTitle(title)}
+          placeholder="New deck Title"
+        />
+        <Button
+          onPress={() => {
+            if (title.trim() !== "") {
+              dispatch(
+                addDeck({ id: Date.now(), title: title.trim(), questions: [] })
+              );
+            } else {
+              Alert.alert(
+                "Deck Title is empty!",
+                "Deck Title must contain at least 1 character",
+                [{ text: "ok" }]
+              );
+            }
+          }}
+          title="Create a deck"
+          style={styles.button}
+          accessibilityLabel="Add a new deck"
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 const styles = StyleSheet.create({
